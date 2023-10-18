981433d2ca2752d1254843d708671db03bc3a6cd
LBK <bakhaipth@gmail.com>
2023-10-18 22:24:12 +0700

    MCTS

---
+++ connect6/Node.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connect6
+{
+    public class Node
+    {
+        public State State { get; set; }
+        public Node Parent { get; set; }
+        public List<Node> ChildArray { get; set; }
+        public List<Node> nodeList = new List<Node>();
+       
+        public Node()
+        {
+
+        }
+        public Node(State state)
+        {
+            this.State = state;
+            this.Parent = null;
+            this.ChildArray = new List<Node>();
+        }
+        public State GetState()
+        {
+            return State;
+        }
+        public Node GetParent()
+        {
+            return this.Parent;
+        }
+        public void SetParent(Node parent)
+        {
+            this.Parent = parent;
+        }
+
+        public List<Node> GetChildArray()
+        {
+
+            return ChildArray;
+        }
+
+        public void SetChildArray(List<Node> childArray)
+        {
+            this.ChildArray = childArray;
+        }
+        public Node GetChildWithMaxScore()
+        {
+        
+            if (ChildArray.Count == 0)
+            {
+                return null;
+            }
+
+            Node maxScoreNode = ChildArray[0];
+            double maxScore = maxScoreNode.State.WinScore;
+
+            foreach (var childNode in ChildArray)
+            {
+                if (childNode.State.WinScore > maxScore)
+                {
+                    maxScoreNode = childNode;
+                    maxScore = childNode.State.WinScore;
+                }
+            }
+
+            return maxScoreNode;
+        }
+        public Node GetRandomChildNode()
+        {
+            // Thực hiện logic để lấy một con ngẫu nhiên từ danh sách các con
+            Random random = new Random();
+            int randomIndex = random.Next(0, ChildArray.Count);
+            return ChildArray[randomIndex];
+        }
+
+
+    }
+}
---
+++ connect6/UCT.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connect6
+{
+    
+    public class UCT
+    {
+        
+        private Node SelectPromisingNode(Node rootNode)
+        {
+            Node node = rootNode;
+            while (node.GetChildArray().Count != 0)
+            {
+                node = UCT.FindBestNodeWithUCT(node);
+            }
+            return node;
+        }
+       private void ExpandNode(Node node)
+{
+    List<State> possibleStates = node.GetState().GetAllPossibleStates();
+    foreach (State state in possibleStates)
+    {
+        Node newNode = new Node(state);
+        newNode.SetParent(node);
+        newNode.GetState().SetPlayerNo(node.GetState().GetOpponent());
+        node.GetChildArray().Add(newNode);
+    }
+}
+
+        public static double UctValue(int totalVisit, double nodeWinScore, int nodeVisit)
+        {
+            if (nodeVisit == 0)
+            {
+                return double.MaxValue;
+            }
+            return (nodeWinScore / (double)nodeVisit) + 1.41 * Math.Sqrt(Math.Log(totalVisit) / (double)nodeVisit);
+        }
+
+        public static Node FindBestNodeWithUCT(Node node)
+        {
+            int parentVisit = node.GetState().GetVisitCount();
+            return node.GetChildArray().OrderByDescending(c => UctValue(parentVisit, c.GetState().GetWinScore(), c.GetState().GetVisitCount())).First();
+        }
+        
+
+       
+    }
+    
+}
